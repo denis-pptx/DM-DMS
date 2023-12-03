dcc42259e932d9d3d08294b378ed411e2e7863ff
Denis-PC <denis.pptx@gmail.com>
2023-12-03 15:49:54 +0300

    books by params procedure

--- scripts/procedures.sql
+++ scripts/procedures.sql
@@ -29,3 +29,32 @@ DELIMITER ;
 
 # Test procedure
 # CALL CreateUser("denis_test", "test_password", "qwe@mail.ru", 1, 0, 0);
+
+
+
+
+# Books by parameters procedure
+DELIMITER //
+CREATE PROCEDURE GetBooksByParameters(
+    IN p_language_id INT,
+    IN p_author_id INT,
+    IN p_genre_id INT,
+    IN p_publisher_id INT
+)
+BEGIN
+    SELECT DISTINCT b.*
+    FROM book b
+    LEFT JOIN book_language bl ON b.id = bl.book_id
+    LEFT JOIN book_author ba ON b.id = ba.book_id
+    LEFT JOIN book_genre bg ON b.id = bg.book_id
+    WHERE
+        (p_language_id IS NULL OR bl.language_id = p_language_id)
+        AND (p_author_id IS NULL OR ba.author_id = p_author_id)
+        AND (p_genre_id IS NULL OR bg.genre_id = p_genre_id)
+        AND (p_publisher_id IS NULL OR b.publisher_id = p_publisher_id);
+END;
+//
+DELIMITER ;
+
+# Test procedure
+# CALL GetBooksByParameters(NULL, 1, 3, NULL);
