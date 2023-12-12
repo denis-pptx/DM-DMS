9c9b067c25cdbac6b09bc986628a7a689fa0dda0
Denis-PC <denis.pptx@gmail.com>
2023-12-12 17:58:32 +0300

    book format crud

--- Library-API/src/router/index.js
+++ Library-API/src/router/index.js
@@ -5,5 +5,6 @@ router.use('/genre', require('./genre-router'));
 router.use('/publisher', require('./publisher-router'));
 router.use('/author', require('./author-router'));
 router.use('/language', require('./language-router'));
+router.use('/book_format', require('./book-format-router'));
 
 module.exports = router;
