4d49c621ee7840a5ca814dd8f8e483e5b04e51ae
Denis-PC <denis.pptx@gmail.com>
2023-12-12 16:46:00 +0300

    author crud

--- Library-API/src/router/index.js
+++ Library-API/src/router/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/auth', require('./auth-router'));
 router.use('/genre', require('./genre-router'));
 router.use('/publisher', require('./publisher-router'));
+router.use('/author', require('./author-router'));
 
 module.exports = router;
